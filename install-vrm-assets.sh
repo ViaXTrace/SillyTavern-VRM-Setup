e12bb7bd4fafea7a066e83afca6efa732dc27ee5
ViaX: Trace <yankeyank90@gmail.com>
2026-05-06 21:38:58 -0300

    fix: rename TMPDIR to INSTALL_TMP to avoid Termux env var conflict

--- install-vrm-assets.sh
+++ install-vrm-assets.sh
@@ -5,9 +5,9 @@
 set -e
 echo "Installing VRM Assets..."
 
-# Use local temp dir to avoid /tmp permission issues
-TMPDIR="$(pwd)/.vrm-install-tmp"
-mkdir -p "$TMPDIR"
+# Use local temp dir (avoids /tmp permission issues on Termux/Android)
+INSTALL_TMP="$(pwd)/.vrm-install-tmp"
+mkdir -p "$INSTALL_TMP"
 
 # Create target directories
 mkdir -p data/default-user/assets/vrm/model
@@ -16,15 +16,15 @@ mkdir -p public/scripts/extensions/third-party
 
 # Clone VRM Assets Pack
 echo "Cloning VRM Assets Pack..."
-git clone --depth=1 https://github.com/test157t/VRM-Assets-Pack-For-Silly-Tavern "$TMPDIR/vrm-assets"
-cp "$TMPDIR/vrm-assets/model/"*.vrm data/default-user/assets/vrm/model/
-cp "$TMPDIR/vrm-assets/animation/"*.bvh data/default-user/assets/vrm/animation/
-echo "VRM Assets installed: $(ls data/default-user/assets/vrm/model/*.vrm | wc -l) models, $(ls data/default-user/assets/vrm/animation/*.bvh | wc -l) animations"
+git clone --depth=1 https://github.com/test157t/VRM-Assets-Pack-For-Silly-Tavern "$INSTALL_TMP/vrm-assets"
+cp "$INSTALL_TMP/vrm-assets/model/"*.vrm data/default-user/assets/vrm/model/
+cp "$INSTALL_TMP/vrm-assets/animation/"*.bvh data/default-user/assets/vrm/animation/
+echo "VRM Assets: $(ls data/default-user/assets/vrm/model/*.vrm | wc -l) models, $(ls data/default-user/assets/vrm/animation/*.bvh | wc -l) animations"
 
 # Clone Spicy VRM Animations
 echo "Cloning Spicy VRM Animations..."
-git clone --depth=1 https://github.com/test157t/Spicy-VRM-Animations "$TMPDIR/spicy-vrm"
-cp "$TMPDIR/spicy-vrm/animation/"*.bvh data/default-user/assets/vrm/animation/
+git clone --depth=1 https://github.com/test157t/Spicy-VRM-Animations "$INSTALL_TMP/spicy-vrm"
+cp "$INSTALL_TMP/spicy-vrm/animation/"*.bvh data/default-user/assets/vrm/animation/
 echo "Total animations: $(ls data/default-user/assets/vrm/animation/*.bvh | wc -l)"
 
 # Clone Voiceforge Extension
@@ -33,7 +33,6 @@ git clone --depth=1 https://github.com/test157t/Extension-Voiceforge public/scri
 echo "Voiceforge installed"
 
 # Cleanup
-rm -rf "$TMPDIR"
+rm -rf "$INSTALL_TMP"
 echo ""
 echo "All done! Start SillyTavern and go to Extensions to enable VoiceForge."
-
